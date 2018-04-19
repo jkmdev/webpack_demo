a2ce3d15100d08ef9c8e7f341f8df248e5b0981e
Julia <jkmcgeoghan@myseneca.ca>
2018-04-18 21:16:28 -0400

    Add image asset

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 
 import _ from 'lodash';
 import './style.css';
+import Icon from './icon.png';
 
 function component() {
   var element = document.createElement('div');
@@ -8,6 +9,11 @@ function component() {
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
+
+  var myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
 
   return element;
 }
